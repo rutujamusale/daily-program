c957ebfdcd10e55767a500e0f8c7b09015a99b45
rutujamusale <69392848+rutujamusale@users.noreply.github.com>
2020-08-09 19:57:25 +0530

    Create day010program1.cpp

---
+++ day010program1.cpp
@@ -0,0 +1,29 @@
+[11:22 PM, 8/8/2020] ~~ Rutuja: 1. Write a program to print half pyramid using numbers.
+Sample output:
+
+1 
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+----------------------------------------
+[11:22 PM, 8/8/2020] ~~ Rutuja: #include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+    
+    cout<<"Enter number of rows: ";
+    cin>>rows;
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<< j <<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
