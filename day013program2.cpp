330fece17c193861f4c69e0e3ef4c28a3b152651
rutujamusale <69392848+rutujamusale@users.noreply.github.com>
2020-08-12 19:30:40 +0530

    Update day013program2.cpp

--- day013program2.cpp
+++ day013program2.cpp
@@ -1,17 +1,8 @@
-// Write a program to find maximum between three numbers.
+//Write a program to find maximum number between two numbers.
 #include <iostream>
 using namespace std;
-
-int main()
+void maximum(int num1,int num2,int num3)
 {
-    int num1,num2,num3;
-    cout<<"Enter the number 1 is: ";
-    cin>>num1;
-    cout<<"\nEnter the number 2 is: ";
-    cin>>num2;
-    cout<<"\nEnter the number 3 is: ";
-    cin>>num3;
-    cout<<endl;
     if(num1>num2&&num1>num3)
     {
         cout<< num1 <<" is maximum number.";
@@ -24,5 +15,34 @@ int main()
     {
         cout<< num3 <<" is maximum number.";
     }
+}
+void minimum(int num1,int num2,int num3)
+{
+        if(num1<num2&&num1<num3)
+    {
+        cout<< num1 <<" is minimum number.";
+    }
+    else if(num2<num1&&num2<num3)
+    {
+        cout<< num2 <<" is minimum number.";
+    }
+    else
+    {
+        cout<< num3 <<" is minimum number.";
+    }
+}
+int main()
+{
+    int num1,num2,num3;
+    cout<<"Enter the number 1 is: ";
+    cin>>num1;
+    cout<<"\nEnter the number 2 is: ";
+    cin>>num2;
+    cout<<"\nEnter the number 3 is: ";
+    cin>>num3;
+    cout<<endl;
+    maximum(num1,num2,num3);
+    minimum(num1,num2,num3);
+
     return 0;
 }
