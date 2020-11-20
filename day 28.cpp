d6b5210afb2b40edfa8c9b76d06322bb98e1f675
rutujamusale <69392848+rutujamusale@users.noreply.github.com>
2020-11-20 23:23:47 +0530

    Create day 28.cpp

---
+++ day 28.cpp
@@ -0,0 +1,20 @@
+//https://www.hackerrank.com/challenges/py-if-else/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+if n % 2 == 1:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
