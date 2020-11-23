89bb45279d7ae873a41501b600fd3e96d66bd412
rutujamusale <69392848+rutujamusale@users.noreply.github.com>
2020-11-23 19:10:57 +0530

    Create day 31.cpp

---
+++ day 31.cpp
@@ -0,0 +1,8 @@
+//https://www.hackerrank.com/challenges/python-loops/problem
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    i=0
+    while (i < n):
+        print (i * i)
+        i+=1
